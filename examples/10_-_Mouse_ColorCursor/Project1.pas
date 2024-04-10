101a279be4e57d5d88606bce8df8d2b0f9b00b93
sechshelme <you@example.com>
2024-04-10 16:32:13 +0200

    cursor

--- examples/10_-_Mouse_ColorCursor/Project1.pas
+++ examples/10_-_Mouse_ColorCursor/Project1.pas
@@ -1,40 +1,20 @@
 program Project1;
 
-// https://github.com/libsdl-org/SDL/issues/9496
+// https://wiki.libsdl.org/SDL3/SDL_CreateColorCursor
+//https://wiki.libsdl.org/SDL3/SDL_CreateCursor
 
 uses
   SDL3,
   ctypes;
 
 var
+  win: PSDL_Window;
   renderer: PSDL_Renderer;
-  customBWCursor,  customColorCursor: PSDL_Cursor;
-  cursors: array of PSDL_Cursor;
+  surface: PSDL_Surface;
 
-  procedure LoadCursors;
-  const
-    size = 132;
-  var
-    Data, mask: array[0..size*size * 4 - 1] of TUint8;
-    i: integer;
-  begin
-    for i := 0 to Length(Data) - 1 do begin
-      Data[i] := $3;
-      mask[i] := $bF;
-    end;
-    customBWCursor := SDL_CreateCursor(Data, mask, size, size, 0, 0);
-customColorCursor:=    SDL_CreateColorCursor(nil,0,0);
-
-    SetLength(cursors, SDL_NUM_SYSTEM_CURSORS);
-    for i := 0 to Length(cursors) - 1 do begin
-      cursors[i] := SDL_CreateSystemCursor(i);
-    end;
-  end;
+   customColorCursor: PSDL_Cursor;
 
   procedure Init;
-  var
-    win: PSDL_Window;
-
   begin
     SDL_Init(SDL_INIT_VIDEO);
     win := SDL_CreateWindow('Cursor', 640, 480, SDL_WINDOW_RESIZABLE);
@@ -49,14 +29,24 @@ customColorCursor:=    SDL_CreateColorCursor(nil,0,0);
       Halt(-1);
     end;
 
-    LoadCursors;
+    surface:=SDL_LoadBMP('mauer.bmp');
+    if surface = nil then begin
+      SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, 'Kann kein BMP nicht laden !');
+      Halt(-1);
+    end;
+
+    customColorCursor:=SDL_CreateColorCursor(surface,0,0);
+    if customColorCursor = nil then begin
+      SDL_LogError(SDL_LOG_CATEGORY_CUSTOM, 'Kann kein Cursor laden !');
+      Halt(-1);
+    end;
+    SDL_SetCursor(customColorCursor);
   end;
 
   procedure Render;
   begin
-    SDL_SetRenderDrawColor(renderer, $99, $99, $9, 255);
+    SDL_SetRenderDrawColor(renderer, $FF, $80, $80, 255);
     SDL_RenderClear(renderer);
-
     SDL_RenderPresent(renderer);
   end;
 
@@ -65,7 +55,6 @@ customColorCursor:=    SDL_CreateColorCursor(nil,0,0);
     quit: boolean = False;
     e: TSDL_Event;
     sym: TSDL_KeyCode;
-    cursorIndex: integer = 0;
   begin
     while not quit do begin
       while SDL_PollEvent(@e) <> 0 do begin
@@ -73,15 +62,8 @@ customColorCursor:=    SDL_CreateColorCursor(nil,0,0);
           SDL_EVENT_MOUSE_BUTTON_DOWN: begin
             case e.button.button of
               SDL_BUTTON_LEFT: begin
-                Inc(cursorIndex);
-                if cursorIndex >= SDL_NUM_SYSTEM_CURSORS then begin
-                  cursorIndex := 0;
-                end;
-
-                SDL_SetCursor(cursors[cursorIndex]);
               end;
               SDL_BUTTON_RIGHT: begin
-                SDL_SetCursor(customBWCursor);
               end;
             end;
             WriteLn('dows');
@@ -105,14 +87,11 @@ customColorCursor:=    SDL_CreateColorCursor(nil,0,0);
   end;
 
   procedure Destroy;
-  var
-    i: integer;
   begin
-    for i := 0 to Length(cursors) - 1 do begin
-      SDL_DestroyCursor(cursors[i]);
-    end;
-    SDL_DestroyCursor(customBWCursor);
     SDL_DestroyCursor(customColorCursor);
+    SDL_DestroySurface(surface);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(win);
   end;
 
 begin
