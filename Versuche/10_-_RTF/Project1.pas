74a8d5620ba6e05f6d7349e32d47412213f47ef1
sechshelme <you@example.com>
2024-04-20 16:24:51 +0200

    rtf

--- Versuche/10_-_RTF/Project1.pas
+++ Versuche/10_-_RTF/Project1.pas
@@ -6,7 +6,7 @@ uses
   ctypes,
   SDL3,
   SDL_ttf,
-  SDL_rtf;
+  SDL_rtf, RTF_Tools;
 
 var
   window: PSDL_Window;
@@ -105,13 +105,51 @@ var
     end;
   end;
 
+  function UTF8_to_UNICODE(_utf8: PChar; advance: pcint): TUint32;
+  var
+    ch: TUint32;
+    i: cint = 0;
+    utf8: pbyte;
+  begin
+    utf8 := pbyte(_utf8);
+
+    ch := TUint32(utf8[i]);
+    if ch >= $F0 then begin
+      ch := (utf8[i] and $07) shl 18;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F) shl 12;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F) shl 6;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F);
+    end else if ch >= $E0 then begin
+      ch := (utf8[i] and $3F) shl 12;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F) shl 6;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F);
+    end else if ch >= $C0 then begin
+      ch := ch or (utf8[i] and $3F) shl 6;
+      Inc(i);
+      ch := ch or (utf8[i] and $3F);
+    end;
+
+    advance^ := i + 1;
+    Result := ch;
+  end;
+
   function CreateFont(Name: PChar; family: TRTF_FontFamily; charset: longint; size: longint; style: longint): pointer; cdecl;
   var
     font: PTTF_Font;
+    TTFstyle: integer;
   begin
     font := TTF_OpenFont('lazy.ttf', size);
     if font = nil then begin
       SDL_Log('Kann kein Font laden !    %s', TTF_GetError);
+    end else begin
+      //      TTFstyle:=TTF_STYLE_NORMAL;
+      //      if style and RTF_FontBold>0 then TTFstyle:=TTFstyle or TTF_STYLE_BOLD;
+      TTF_SetFontStyle(font, style);
     end;
 
     WriteLn('CreateFont io.');
@@ -119,32 +157,39 @@ var
   end;
 
   function GetLineSpacing(font: pointer): longint; cdecl;
-  var
-    font2: PTTF_Font;
   begin
-    font2 := PTTF_Font(font);
-    Result := TTF_FontLineSkip(font2);
+    Result := TTF_FontLineSkip(font);
     WriteLn('GetLineSpacing io.');
   end;
 
   function GetCharacterOffsets(font: pointer; Text: PChar; byteOffsets: Plongint; pixelOffsets: Plongint; maxOffsets: longint): longint; cdecl;
   var
     i: cint = 0;
+    bytes: cint = 0;
+    pixels: cint = 0;
+    advance: cint;
+    ch: TUint16;
   begin
+
     while (Text^ <> #0) and (i < maxOffsets) do begin
-      byteOffsets[i] := i;
-      pixelOffsets[i] := i;
+      byteOffsets[i] := bytes;
+      pixelOffsets[i] := pixels;
       Inc(i);
 
-      Inc(Text, 1);
+      ch := UTF8_to_UNICODE(Text, @advance);
+      Inc(Text, advance);
+      Inc(bytes, advance);
+
+      TTF_GlyphMetrics(font, ch, nil, nil, nil, nil, @advance);
+      Inc(pixels, advance);
     end;
 
     if i < maxOffsets then begin
-      byteOffsets[i] := i;
-      pixelOffsets[i] := i;
+      byteOffsets[i] := bytes;
+      pixelOffsets[i] := pixels;
     end;
 
-    SDL_Log('offset: %i',i );
+    SDL_Log('offset: %i', i);
     SDL_Log('max offset: %i', maxOffsets);
     Result := i;
   end;
@@ -156,11 +201,8 @@ var
   begin
     font2 := PTTF_Font(font);
 
-    WriteLn('font2: ', PtrUInt(Text));
-    WriteLn('font2: ', PtrUInt(renderer));
-
     WriteLn(Text);
-//    Text := 'sdfgffdsgsfd';
+    //    Text := 'sdfgffdsgsfd';
     WriteLn(Text);
 
     Result := nil;
@@ -177,8 +219,8 @@ var
 
   procedure FreeFont(font: pointer); cdecl;
   begin
-    WriteLn('CloseFont');
     TTF_CloseFont(font);
+    WriteLn('FreeFont');
   end;
 
 begin
---
+++ Versuche/10_-_RTF/rtf_tools.pas
@@ -0,0 +1,15 @@
+unit RTF_Tools;
+
+interface
+
+function GetRTF_ctx: PRTF_Context;
+
+implementation
+
+function GetRTF_ctx: PRTF_Context;
+begin
+
+end;
+
+end.
+
