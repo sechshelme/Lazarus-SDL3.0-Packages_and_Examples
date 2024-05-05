35a77c0b5410fbdaf8336bec2e14213a6f64403b
sechshelme <you@example.com>
2024-05-05 16:37:52 +0200

    surface

---
+++ examples/10_-_Surface/Project1.pas
@@ -0,0 +1,39 @@
+program Project1;
+
+// https://wiki.libsdl.org/SDL3/SDL_GetWindowSurface
+
+uses
+  SDL3;
+
+var
+  window: PSDL_Window;
+  screen, image: PSDL_Surface;
+begin
+  if SDL_init(SDL_INIT_VIDEO) < 0 then begin
+    SDL_Log('Konnte SDL-VIDEO nicht laden!:  %s', SDL_GetError);
+  end;
+
+  window := SDL_CreateWindow('Surface Example', 640, 480, 0);
+  if window = nil then begin
+    SDL_Log('Konnte kein Windows erzeugen!:  %s', SDL_GetError);
+  end;
+
+  screen := SDL_GetWindowSurface(window);
+  if screen = nil then begin
+    SDL_Log('Konnte kein Surface erzeugen!:  %s', SDL_GetError);
+  end;
+
+  image := SDL_LoadBMP('mauer.bmp');
+  if image = nil then begin
+    SDL_Log('Konnte BMP nicht laden!:  %s', SDL_GetError);
+  end;
+
+  SDL_BlitSurface(image, nil, screen, nil);
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(5000);
+
+  SDL_DestroySurface(image);
+  SDL_DestroyWindow(window);
+  SDL_Quit;
+end.
