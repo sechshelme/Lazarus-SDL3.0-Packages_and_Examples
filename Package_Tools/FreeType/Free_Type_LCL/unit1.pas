74622d6d8bcb0c7dd4b7365cd015ae563fcc05dc
sechshelme <you@example.com>
2024-05-12 19:57:34 +0200

    font

--- Package_Tools/FreeType/Free_Type_LCL/unit1.pas
+++ Package_Tools/FreeType/Free_Type_LCL/unit1.pas
@@ -19,11 +19,13 @@ type
     Timer1: TTimer;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure FormResize(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
     library_: TFT_Library;
     face: TFT_Face;
 
+    procedure draw_bitmap(bit: PFT_Bitmap; x: TFT_Int; y: TFT_Int);
     procedure Face_To_Image(angle: single);
   public
   end;
@@ -35,26 +37,24 @@ implementation
 
 {$R *.lfm}
 
-const
-  ImgWidth = 1280;
-  ImgHeight = 960;
-
 var
-  image: array [0..ImgHeight - 1, 0..ImgWidth - 1] of char;
-
+  image: array of byte = nil;
 
 procedure TForm1.FormCreate(Sender: TObject);
 const
-  fileName = '/usr/share/fonts/truetype/freefont/FreeMono.ttf';
-  //    fileName2 = '/usr/share/wine/fonts/courier.ttf';
+//  fileName = '/usr/share/fonts/truetype/freefont/FreeMono.ttf';
+//  fileName = '/usr/share/fonts/truetype/noto/NotoSansMono-Bold.ttf';
+      fileName = '/usr/share/fonts/truetype/ubuntu/Ubuntu-MI.ttf';
 var
   error: TFT_Error;
 begin
   Timer1.Enabled := False;
   Timer1.Interval := 100;
-  ClientWidth := ImgWidth + 50;
-  ClientHeight := ImgHeight + 50;
+  ClientWidth := 1600;
+  ClientHeight := 1200;
 
+  OpenGLControl1.Align := alClient;
+  OpenGLControl1.AutoResizeViewport:=False;
   OpenGLControl1.MakeCurrent();
   glClearColor(0, 0, 0, 0);
 
@@ -68,7 +68,7 @@ begin
     WriteLn('Fehler: ', error);
   end;
 
-  error := FT_Set_Char_Size(face, 2500, 0, 150, 0);
+  error := FT_Set_Char_Size(face, 5000, 00, 0, 350);
   if error <> 0 then begin
     WriteLn('Fehler: Set_Char_Size   ', error);
   end;
@@ -82,18 +82,27 @@ begin
   FT_Done_FreeType(library_);
 end;
 
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  Timer1.Enabled := False;
+  SetLength(image, OpenGLControl1.Width * OpenGLControl1.Height);
+  WriteLn('OpenGlControl: ', OpenGLControl1.Width, ' x ', OpenGLControl1.Height, '     Form: ', Width, ' x ', Height);
+  FillChar(image[0], Length(image), 0);
+  Timer1.Enabled := True;
+end;
+
 procedure TForm1.Timer1Timer(Sender: TObject);
 const
   angle: single = 0.0;
 begin
-  angle += 0.1;
+  angle += 0.3;
   Face_To_Image(angle);
 
-  glDrawPixels(ImgWidth, ImgHeight, GL_LUMINANCE, GL_UNSIGNED_BYTE, @image);
+  glDrawPixels(OpenGLControl1.Width, OpenGLControl1.Height, GL_LUMINANCE, GL_UNSIGNED_BYTE, Pointer(image));
   OpenGLControl1.SwapBuffers;
 end;
 
-procedure draw_bitmap(bit: PFT_Bitmap; x: TFT_Int; y: TFT_Int);
+procedure TForm1.draw_bitmap(bit: PFT_Bitmap; x: TFT_Int; y: TFT_Int);
 var
   x_max, y_max, i, j, p, q: TFT_Int;
 begin
@@ -101,23 +110,22 @@ begin
   y_max := y + bit^.rows;
 
   i := x;
-  p := 0;
+  p := -1;
   while (i < x_max) do begin
     Inc(i);
     Inc(p);
 
     j := y;
-    q := 0;
+    q := -1;
     while (j < y_max) do begin
       Inc(j);
       Inc(q);
 
-      if (i < 0) or (j < 0) or (i >= ImgWidth) or (j >= ImgHeight) then begin
+      if (i < 0) or (j < 0) or (i >= OpenGLControl1.Width) or (j >= OpenGLControl1.Height) then begin
         Continue;
       end;
 
-    //  image[j, ImgHeight - i] := char(byte(image[j, ImgHeight - i]) or byte(bit^.buffer[q * bit^.Width + p]));
-            image[j, i] :=char(byte(image[j, i])or byte(bit^.buffer[q * bit^.Width + p]));
+      image[j * OpenGLControl1.Width + i] := image[j * OpenGLControl1.Width + i] or bit^.buffer[q * bit^.Width + p];
     end;
   end;
 end;
@@ -136,14 +144,10 @@ var
 begin
   slot := face^.glyph;
 
-  angle += pi / 7;
-  WriteLn(angle: 4: 2);
-//  angle := pi;
-
-  matrix.xx := Round(Cos(angle) * $10000);
-  matrix.xy := Round(-Sin(angle) * $10000);
-  matrix.yx := Round(Sin(angle) * $10000);
-  matrix.yy := Round(Cos(angle) * $10000);
+  matrix.xx := Round(Cos(angle) * 10000);
+  matrix.xy := -Round(-Sin(angle) * 10000);
+  matrix.yx := Round(Sin(angle) * 10000);
+  matrix.yy := -Round(Cos(angle) * 10000);
 
   pen.x := 40000;
   pen.y := 50000;
@@ -156,7 +160,7 @@ begin
       WriteLn('Fehler: Load_Char   ', error);
     end;
 
-    draw_bitmap(@slot^.bitmap, slot^.bitmap_left, ImgHeight - slot^.bitmap_top);
+    draw_bitmap(@slot^.bitmap, slot^.bitmap_left, OpenGLControl1.Height - slot^.bitmap_top);
 
     pen.x += slot^.advance.x;
     pen.y += slot^.advance.y;
