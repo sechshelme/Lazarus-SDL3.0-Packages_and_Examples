58e212eabbaa09afddf85a171e7682e90ddcf36b
sechshelme <you@example.com>
2024-05-17 20:10:30 +0200

    opengl

---
+++ examples/SDL_OpenGL/SDL_GL_ExtensionSupported/Project1.pas
@@ -0,0 +1,22 @@
+program Project1;
+
+uses
+  SDL3;
+
+  // https://wiki.libsdl.org/SDL3/SDL_GL_ExtensionSupported
+
+
+  procedure ShowGLExt(const ext:String);
+  begin
+    SDL_Log(PChar( ext + ' %d: ') ,SDL_GL_ExtensionSupported(PChar(ext)));
+  end;
+
+
+begin
+  SDL_Init(SDL_INIT_VIDEO);
+
+  ShowGLExt('GLX_ARB_create_context');
+  ShowGLExt('GL_3DFX_texture_compression_FXT1');
+
+  SDL_Quit;
+end.
