c8fb338d6b64333045dc75733baa6b56cfcf8644
sechshelme <you@example.com>
2024-05-30 18:13:05 +0200

    create_surface

--- examples/Rendering/10_-_Surface/Project1.pas
+++ examples/Rendering/10_-_Surface/Project1.pas
@@ -3,7 +3,7 @@ program Project1;
 uses
   SDL3;
 
-  function CreateSurface: PSDL_Surface;
+  function CreateSurface1: PSDL_Surface;
   begin
     Result := SDL_LoadBMP('mauer.bmp');
     if Result = nil then begin
@@ -11,10 +11,26 @@ uses
     end;
   end;
 
+  function CreateSurface2: PSDL_Surface;
+  const
+    Data: array of DWord = (
+      $000000FF, $FF0000FF, $00FF00FF, $0000FFFF,
+      $444444FF, $FF4444FF, $44FF44FF, $4444FFFF,
+      $888888FF, $FF8888FF, $88FF88FF, $8888FFFF,
+      $AAAAAAFF, $FFAAAAFF, $AAFFAAFF, $AAAAAAFF);
+  begin
+    Result := SDL_CreateSurfaceFrom(PDWord(Data), 4, 4, 16, SDL_PIXELFORMAT_RGBA8888);
+    if Result = nil then begin
+      SDL_Log('Konnte BMP nicht laden!:  %s', SDL_GetError);
+    end;
+    WriteLn(PtrUInt(@data));
+    WriteLn(PtrUInt(Result^.pixels));
+  end;
+
   procedure main;
   var
     window: PSDL_Window;
-    winSurface, imageSurface: PSDL_Surface;
+    winSurface, imageSurface1, imageSurface2: PSDL_Surface;
     rDest: TSDL_Rect;
     quit: boolean = False;
     event: TSDL_Event;
@@ -33,7 +49,8 @@ uses
       SDL_Log('Konnte kein Surface erzeugen!:  %s', SDL_GetError);
     end;
 
-    imageSurface := CreateSurface;
+    imageSurface1 := CreateSurface1;
+    imageSurface2 := CreateSurface2;
 
     while not quit do begin
       while SDL_PollEvent(@event) do begin
@@ -52,12 +69,14 @@ uses
       end;
 
       rDest.items := [100, 100, 200, 200];
-      SDL_BlitSurfaceScaled(imageSurface, nil, winSurface, @rDest, SDL_SCALEMODE_NEAREST);
-      SDL_BlitSurface(imageSurface, nil, winSurface, nil);
+      SDL_BlitSurfaceScaled(imageSurface1, nil, winSurface, @rDest, SDL_SCALEMODE_NEAREST);
+      rDest.items := [350, 100, 200, 200];
+      SDL_BlitSurfaceScaled(imageSurface2, nil, winSurface, @rDest, SDL_SCALEMODE_NEAREST);
       SDL_UpdateWindowSurface(window);
     end;
 
-    SDL_DestroySurface(imageSurface);
+    SDL_DestroySurface(imageSurface1);
+    SDL_DestroySurface(imageSurface2);
     SDL_DestroySurface(winSurface);
     SDL_DestroyWindow(window);
     SDL_Quit;
