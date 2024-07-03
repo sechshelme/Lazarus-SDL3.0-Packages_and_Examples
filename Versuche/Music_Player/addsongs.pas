a6d0625ae75d657c543b04ee686b7c6fb0f92694
sechshelme <you@example.com>
2024-07-03 17:51:18 +0200

    emcc

--- Versuche/Music_Player/addsongs.pas
+++ Versuche/Music_Player/addsongs.pas
@@ -6,21 +6,18 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ShellCtrls, StdCtrls,
- Buttons, ComCtrls, FileUtil;
+  Buttons, ComCtrls, FileUtil;
 
 type
-
-  { TAddSoundForm }
-
   TAddSoundForm = class(TForm)
     procedure FormCreate(Sender: TObject);
   private
     btn: TBitBtn;
-        ListBox:TListBox;
+    ListBox: TListBox;
 
-        procedure btnClick(Sender: TObject);
-        procedure btnCloseClick(Sender: TObject);
-        function FindFile(const path:string):TStringList;
+    procedure btnClick(Sender: TObject);
+    procedure btnCloseClick(Sender: TObject);
+    function FindFile(const path: string): TStringList;
   public
 
   end;
@@ -33,39 +30,37 @@ implementation
 uses
   Unit1;
 
-{$R *.lfm}
-
-{ TAddSoundForm }
+  {$R *.lfm}
 
 procedure TAddSoundForm.FormCreate(Sender: TObject);
 var
   sl: TStringList;
 begin
-btn:=   TBitBtn.Create(self);
-btn.Parent:=Self;
-btn.Anchors:=[akBottom,akRight];
-btn.Left:=ClientWidth-90;
-btn.Top:=ClientHeight-100;
-btn.OnClick:=@btnClick;
-btn.Caption:='Add';
+  btn := TBitBtn.Create(self);
+  btn.Parent := Self;
+  btn.Anchors := [akBottom, akRight];
+  btn.Left := ClientWidth - 90;
+  btn.Top := ClientHeight - 100;
+  btn.OnClick := @btnClick;
+  btn.Caption := 'Add';
 
-btn:=   TBitBtn.Create(self);
-btn.Parent:=Self;
-btn.Anchors:=[akBottom,akRight];
-btn.Left:=ClientWidth-90;
-btn.Top:=ClientHeight-50;
-btn.OnClick:=@btnCloseClick;
-btn.Caption:='Close';
+  btn := TBitBtn.Create(self);
+  btn.Parent := Self;
+  btn.Anchors := [akBottom, akRight];
+  btn.Left := ClientWidth - 90;
+  btn.Top := ClientHeight - 50;
+  btn.OnClick := @btnCloseClick;
+  btn.Caption := 'Close';
 
-  ListBox:=TListBox.Create(Self);
-  ListBox.Parent:=Self;
-  ListBox.Left:=10;
-  ListBox.Top:=10;
-  ListBox.Height:=ClientHeight-20;
-  ListBox.Width:=ClientWidth-110;
-  ListBox.Anchors:=[akLeft,akTop,akBottom,akRight];
+  ListBox := TListBox.Create(Self);
+  ListBox.Parent := Self;
+  ListBox.Left := 10;
+  ListBox.Top := 10;
+  ListBox.Height := ClientHeight - 20;
+  ListBox.Width := ClientWidth - 110;
+  ListBox.Anchors := [akLeft, akTop, akBottom, akRight];
 
-  sl:=FindFile('/n4800/Multimedia/Music/Disco/Boney M/1981 - Boonoonoonoos');
+  sl := FindFile('/n4800/Multimedia/Music/Disco/Boney M/1981 - Boonoonoonoos');
   ListBox.Clear;
   ListBox.Items.AddStrings(sl);
   sl.Free;
@@ -75,17 +70,18 @@ function TAddSoundForm.FindFile(const path: string): TStringList;
 var
   Info: TRawByteSearchRec;
 begin
-  Result:=TStringList.Create;
-if  FindFirst(path+'/*.flac',faAnyFile,Info)=0 then begin
-  repeat
-        Result.Add(path+'/'+ Info.Name);
-  until FindNext(Info)<>0;
-end;
+  Result := TStringList.Create;
+  if FindFirst(path + '/*.flac', faAnyFile, Info) = 0 then begin
+    repeat
+      Result.Add(path + '/' + Info.Name);
+    until FindNext(Info) <> 0;
+  end;
+  FindClose(Info);
 end;
 
 procedure TAddSoundForm.btnClick(Sender: TObject);
 begin
- form1.ListBoxSongs.Items:=ListBox.Items;
+  form1.ListBoxSongs.Items := ListBox.Items;
 end;
 
 procedure TAddSoundForm.btnCloseClick(Sender: TObject);
@@ -94,4 +90,3 @@ begin
 end;
 
 end.
-
---
+++ emscripten/readme.md
@@ -0,0 +1,27 @@
+# Einleitung
+
+https://emscripten.org/docs/getting_started/Tutorial.html
+
+# Installieren
+```bash
+sudo apt install emscripten
+```
+
+# Eine HelloWorld kompilieren
+
+```bash
+emcc hello.c -o hello.html
+```
+
+# Localhist-Server starten
+
+```bash
+python3 -m http.server
+```
+
+# Browser Ansicht.
+
+Im Broweser: `http://localhost:8000/`
+
+
+
